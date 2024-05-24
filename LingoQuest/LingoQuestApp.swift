72dbd8f287500de709c16a3592212ecd5e78227b
Louis Mario <louism4rio@gmail.com>
2024-05-24 09:33:06 +0700

    Initial Commit

---
+++ LingoQuest/LingoQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoQuestApp.swift
+//  LingoQuest
+//
+//  Created by MacBook Pro on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LingoQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
