a42b1ff4c9f44412d01430304e70ec1ff52ff947
Louis Mario <louism4rio@gmail.com>
2024-05-24 09:42:56 +0700

    Mac

---
+++ LingoQuest_Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LingoQuest_Mac
+//
+//  Created by MacBook Pro on 24/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LingoQuest_Mac/LingoQuest_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoQuest_MacApp.swift
+//  LingoQuest_Mac
+//
+//  Created by MacBook Pro on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LingoQuest_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
